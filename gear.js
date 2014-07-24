fb3cbdc39d590c8dd858a4d635bbe3d563225bb1
aslinwang <dreamjet.wang@gmail.com>
2014-07-24 16:26:59 +0800

    first commit

---
+++ gear.js
@@ -0,0 +1,44 @@
+//继承underscore
+var _ = require('underscore');
+_.extend(exports, _);
+
+//继承q（Promise Library）
+var q = require('q');
+_.extend(exports, {q:q});
+
+//并发处理
+var Do = require('./lib/do');
+exports.Do = Do;
+exports.do = function(fn){
+  var d = new Do();
+  if(fn){
+    return d.do(fn);
+  }
+  else{
+    return d;
+  }
+}
+
+//职责链对象
+var Connect = require('./lib/connect');
+exports.Connect = Connect;
+exports.connect = function(){
+  return new Connect();
+}
+
+//文件操作类
+exports.fs = require('./lib/fs');
+
+//数据监听
+exports.listen = require('./lib/listen').listen;
+
+//计算简单对象长度
+exports.jsonsize = function(json){
+  var size = 0;
+  for(var key in json){
+    if(json.hasOwnProperty(key)){
+      size++;
+    }
+  }
+  return size;
+}
